48b2265c4e512a590b1a6ee1540963fe46dfdbd1
raulfarce01 <raulfarce01@gmail.com>
2024-05-29 17:13:48 +0200

    Arreglo de estilos en mobile

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -20,20 +20,21 @@
         <i id="flechaIndex" class="fa-solid fa-arrow-down text-white text-4xl absolute bottom-8 cursor-pointer"></i>
     </div>
 </div>
-<div id="familia" class="h-screen text-white py-5 px-7 flex flex-col md:flex-row">
-    <div id="textoFamilia" class="pe-10">
-        <h2 class="md:text-4xl text-3xl font-bold pb-10">Nuestra Familia</h2>
+<div id="familia" class="h-screen text-white py-5 md:px-7 ps-5 flex flex-col md:flex-row">
+    <div id="textoFamilia" class="md:pe-10 flex justify-center flex-col">
+        <h2 class="md:text-4xl text-3xl font-bold md:pb-10 pb-5">Nuestra Familia</h2>
+        <img src="{{ asset('img_estaticas/plantilla.png') }}" class="md:w-80 md:h-80 my-5 md:hidden block m-auto" alt="">
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta similique asperiores veniam id corporis! Libero, eos recusandae cupiditate aliquam dolore similique velit fugit? Aut in commodi ab, fugiat libero blanditiis. Culpa at, repellat sapiente assumenda aliquam soluta iure recusandae dicta et corrupti quam! Aliquam enim consequatur tempora ipsum a sunt.
         </p>
         <br>
-        <p class="mb-10">
+        <p class="md:mb-10">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo quos, necessitatibus, voluptatibus exercitationem, eveniet repellat sit est doloribus excepturi illum modi ut quisquam repudiandae officiis. Quod doloremque dolor aspernatur voluptatibus soluta debitis doloribus laboriosam blanditiis temporibus corrupti repudiandae officia, praesentium dignissimos eaque, facilis tempore nobis cum odio. Nemo dicta quasi nobis esse quos hic repudiandae incidunt. Cupiditate error iure assumenda vel nemo placeat ducimus eligendi similique modi odit harum perferendis dolorem voluptatibus voluptates tempora illo laudantium quo, eius sapiente beatae, consequuntur, perspiciatis quibusdam in? Ut sit mollitia dolorum error voluptatem veritatis recusandae ex iste eos necessitatibus voluptate dicta tempora dignissimos eveniet qui ab ratione officiis in rem architecto molestias, reiciendis nemo debitis? Eum veritatis commodi laboriosam nam eveniet ipsam reprehenderit perspiciatis accusantium maxime debitis, repellendus optio sit expedita labore eius iusto unde quidem quis ratione est, facilis nulla nemo amet. Quae adipisci laudantium assumenda repellat, dolores laborum incidunt id quas!
         </p>
         <br>
-        <a href="/sobre" class="bg-white font-bold text-lg text-black px-5 py-2 rounded-full ">Conócenos más a fondo</a>
+        <a href="/sobre" class="bg-white font-bold text-lg text-black px-5 py-2 rounded-full text-center mb-5 md:w-1/3">Conócenos más a fondo</a>
     </div>
-    <img src="{{ asset('img_estaticas/plantilla.png') }}" class="md:w-80 md:h-80 mt-20" alt="">
+    <img src="{{ asset('img_estaticas/plantilla.png') }}" class="md:w-80 md:h-80 mt-20 hidden md:block" alt="">
 
 </div>
 
